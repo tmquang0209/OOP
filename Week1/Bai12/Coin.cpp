06afb7693e3c3b21503ef72a51cec1842717223a
Trần Minh Quang <tmquang0209@gmail.com>
2022-05-07 16:15:57 +0700

    Bai12

---
+++ Week1/Bai12/Coin.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include "Coin.h"
+
+using namespace std;
+
+const int TIMES = 20;
+
+Coin::Coin()
+{
+    toss();
+}
+
+void Coin::toss()
+{
+    int random_number = rand() % 2;
+    sideUp = (random_number == 0) ? "Heads" : "Tails";
+}
+
+string Coin::getSideUp()
+{
+    return sideUp;
+}
+
+int main()
+{
+    Coin coin = Coin();
+    cout << "Mat dong xu hien tai: " << coin.getSideUp() << endl;
+    srand(time(NULL));
+    int count = 0;
+    for (int i = 0; i < TIMES; i++)
+    {
+        coin.toss();
+        cout << "Toss: " << coin.getSideUp() << endl;
+        if (coin.getSideUp() == "Heads")
+        {
+            count++;
+        }
+    }
+    cout << "Total heads: " << count << endl;
+    cout << "Total tails: " << TIMES - count << endl;
+}
