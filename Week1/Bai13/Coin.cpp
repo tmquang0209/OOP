24eab008c4120783511f1c1e745377cc025d2800
Trần Minh Quang <tmquang0209@gmail.com>
2022-05-08 21:37:05 +0700

    Bai 13

--- Week1/Bai13/Coin.cpp
+++ Week1/Bai13/Coin.cpp
@@ -5,7 +5,9 @@
 
 using namespace std;
 
-const int TIMES = 20;
+const double AMOUNT_COIN_1 = 0.25;
+const double AMOUNT_COIN_2 = 0.10;
+const double AMOUNT_COIN_3 = 0.05;
 
 Coin::Coin()
 {
@@ -26,18 +28,40 @@ string Coin::getSideUp()
 int main()
 {
     Coin coin = Coin();
-    cout << "Mat dong xu hien tai: " << coin.getSideUp() << endl;
+
+    double cash = 0;
     srand(time(NULL));
+    cout << "Mat dong xu hien tai: " << coin.getSideUp() << endl;
     int count = 0;
-    for (int i = 0; i < TIMES; i++)
+    int i = 1;
+    while (cash < 100.0)
     {
         coin.toss();
-        cout << "Toss: " << coin.getSideUp() << endl;
-        if (coin.getSideUp() == "Heads")
+        cout << (i % 3) << " Mat dong xu hien tai: " << coin.getSideUp() << "\t";
+        if (coin.getSideUp() == "Heads" && i % 3 == 0)
         {
+            cout << AMOUNT_COIN_3;
+            cash += AMOUNT_COIN_3 * 10;
             count++;
         }
+        else if (coin.getSideUp() == "Heads" && i % 3 == 2)
+        {
+            cout << AMOUNT_COIN_2;
+            cash += AMOUNT_COIN_2 * 10;
+            count++;
+        }
+        else if (coin.getSideUp() == "Heads" && i % 3 == 1)
+        {
+            cout << AMOUNT_COIN_1;
+            cash += AMOUNT_COIN_1 * 10;
+            count++;
+        }
+        cout << endl;
+        i++;
     }
-    cout << "So lan ngua: " << count << endl;
-    cout << "So lan up: " << TIMES - count << endl;
+    cout << "Ban gianh duoc " << cash << " xu. ";
+    if (cash == 100)
+        cout << "Ban da chien thang!";
+    else
+        cout << "Ban thua!";
 }
